fee9d0f35873d309299511cd9487983c9ea3500c
MANOS Poulakakis <SFPQW5G@post.at>
2024-11-13 12:27:53 +0200

    Modified Repositories Logic to Implement Interfaces Generics Logic

--- AppointmentManagementSystem/Data/Repositories/InMemoryAppointmentRepository.cs
+++ AppointmentManagementSystem/Data/Repositories/InMemoryAppointmentRepository.cs
@@ -1,19 +1,17 @@
 using System.Collections.ObjectModel;
 using AppointmentManagementSystem.Data.Abstractions;
 using AppointmentManagementSystem.Data.Models;
-using Spectre.Console;
 
 namespace AppointmentManagementSystem.Data.Repositories
 {
-    public class InMemoryAppointmentRepository(List<Appointment>? initialData = null) : IAppointmentRepository
+    public class InMemoryAppointmentRepository<T>() : IAppointmentRepository<T> where T : Appointment
     {
-         private readonly List<Appointment> _appointments = initialData ?? [];
-
-        public Task CreateAppointment(Appointment appointment)
+        private readonly List<T> _massageAppointments = InitialData.GetInitialAppointments<T>();//[];  //InitialData.GetInitialAppointments<T>();
+        public Task CreateAppointment(T appointment)
         {
             if (appointment is not null)
             {
-                _appointments.Add(appointment);
+                _massageAppointments.Add(appointment);
             }
             return Task.CompletedTask;
         }
@@ -21,32 +19,33 @@ namespace AppointmentManagementSystem.Data.Repositories
         public Task<bool> DeleteAppointment(int id)
         {
             bool isDeleted = false;
-            var foundAppointment = _appointments.Find(x => x.Id == id);
-            isDeleted = foundAppointment is not null && _appointments.Remove(foundAppointment);
+            var foundAppointment = _massageAppointments.Find(x => x.Id == id);
+            isDeleted = foundAppointment is not null && _massageAppointments.Remove(foundAppointment);
             return Task.FromResult(isDeleted);
         }
 
-        public Task<ReadOnlyCollection<Appointment>> GetAppointments() => Task.FromResult(_appointments.AsReadOnly());
+        public Task<ReadOnlyCollection<T>> GetAppointments() => Task.FromResult(_massageAppointments.AsReadOnly());
 
-        public Task UpdateAppointment(Appointment appointment)
+        public Task UpdateAppointment(T appointment)
         {
-            var foundAppointment = _appointments.Find(x => x.Id == appointment.Id);
+            var foundAppointment = _massageAppointments.Find(x => x.Id == appointment.Id);
             if (foundAppointment is not null)
             {
-                foundAppointment.ServiceType = appointment.ServiceType;
+                //foundAppointment.ServiceType = appointment.ServiceType;
                 foundAppointment.AppointmentDate = appointment.AppointmentDate;
                 foundAppointment.AppointmentNotes = appointment.AppointmentNotes;
             }
             return Task.CompletedTask;
         }
 
-        public Task<Appointment> AppointmentExists(int id)
+        public Task<T> AppointmentExists(int id)
         {
-            var foundAppointment = _appointments.Find(x => x.Id == id);
+            var foundAppointment = _massageAppointments.Find(x => x.Id == id);
             if (foundAppointment is not null)
                 return Task.FromResult(foundAppointment);
             else
-                return Task.FromResult<Appointment>(null);
+                return Task.FromResult<T>(null);
         }
+
     }
 }
